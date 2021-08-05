662163b8e8bae7b00a44e0726dffea065398777a
vadim4ik99 <47747472+vadim4ik99@users.noreply.github.com>
2021-08-05 16:39:24 +0300

    4
    4

--- !t05_string_frequency/index.js
+++ !t05_string_frequency/index.js
@@ -4,35 +4,42 @@ class StrFrequency {
   }
 
   letterFrequencies(){
+	let obj = {};
 	let str = this.str.replace(/[^a-zа-яё]/gi, '').toUpperCase();
     str = new Set(str.split(''));
 	for (let value of str) {
 		let count = (this.str.toUpperCase().split(value).length - 1);
-		console.log(`Letter ${value} is repeated ${count} times`);
+		obj[value] = count;
 	}
+	return obj;
   }
 
   wordFrequencies(){
+	let obj = {}; 
 	let words = this.str.toUpperCase().split(" ");
-	words.forEach(function(item, i, arr) {
-		
-	});
+	let str = new Set(words);
+	for (let value of str) {
+		let count = (this.str.toUpperCase().split(value).length - 1);
+		obj[value] = count;	
+	}
+	return obj;
   }
 
   reverseString(){
-	return true;
+	 let result = this.str.split('').reverse().join('');
+	 return result;
   }
 }
 
 function test(str) {
   const sf = new StrFrequency(str);
   console.log(`Letters in ${str}`);
-  for (const [k, v] of Object.entries(sf.letterFrequencies())) {
-      console.log(`Letter ${k} is repeated ${v} times`);
-  }
+   for (const [k, v] of Object.entries(sf.letterFrequencies())) {
+       console.log(`Letter ${k} is repeated ${v} times`);
+	}
 
-  console.log(`Words in ${str}`);
-  for (const [k, v] of Object.entries(sf.wordFrequencies())) {
+   console.log(`Words in ${str}`);
+	for (const [k, v] of Object.entries(sf.wordFrequencies())) {
       console.log(`Word ${k} is repeated ${v} times`);
   }
 
